0b9c129f44f7e0642ed83f88697311798ab23e1d
cawauchi6204 <cawauchi6204@gmail.com>
2020-06-12 13:34:48 +0900

    サインアップ機能の実装

---
+++ Desktop/pitmark/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <span v-if="user">こんにちは {{user.mail }}さん</span>
+    <span v-else>ログインしていません</span>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import firebase from "firebase";
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      user: null
+    };
+  },
+  created() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.user = user;
+    });
+  },
+  components: {}
+};
+</script>
